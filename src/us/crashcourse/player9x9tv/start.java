0633fbf2ae25938ddaa1d7f72ee06f1c0d45ae75
lawrence <lawrence@a0a5f996-2bb4-401c-8778-395c3c01700f>
2014-02-24 18:50:39 +0000

    Added Fan App directories.
    git-svn-id: http://svn.sj.9x9.tv:8080/repos/misc/trunk/mobile/android51a@5948 a0a5f996-2bb4-401c-8778-395c3c01700f

---
+++ src/us/crashcourse/player9x9tv/start.java
@@ -0,0 +1,47 @@
+package us.crashcourse.player9x9tv;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+public class start extends Activity
+	{
+	@Override
+	public void onCreate (Bundle savedInstanceState)
+		{
+		super.onCreate (savedInstanceState);
+		launch_start();
+		}
+	
+	@Override
+	protected void onStart()
+		{
+		super.onStart();
+		}
+
+	@Override
+	protected void onStop()
+		{
+		super.onStop();
+		}
+
+	@Override
+	protected void onDestroy ()
+		{
+		super.onDestroy();
+		}
+	
+	@Override
+	protected void onResume()
+		{
+		super.onResume();
+		}	
+	
+	public void launch_start()
+		{
+		Log.i ("vtest", "VICE Activity, launching start");
+		Intent wIntent = new Intent (this, tv.tv9x9.player.start.class);
+		startActivity (wIntent);
+		}
+	}
