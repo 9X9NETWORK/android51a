5b1f001f21963d5ad8ead6ba4bd3e110318175e3
Soylent <soylent@ubuntu.(none)>
2014-06-10 18:35:54 -0700

    forgot 2 new files. build 113

---
+++ src/tv/tv9x9/player/AppCastDeviceService.java
@@ -0,0 +1,7 @@
+package tv.tv9x9.player;
+
+import com.fireflycast.server.cast.service.CastDeviceService;
+
+public class AppCastDeviceService extends CastDeviceService
+	{
+	}
---
+++ src/tv/tv9x9/player/AppCastService.java
@@ -0,0 +1,7 @@
+package tv.tv9x9.player;
+
+import com.fireflycast.server.cast.service.CastService;
+
+public class AppCastService extends CastService
+	{
+	}
